4915d6b9ca95180393f080ec6d6b8ba40661102a
Ahmed <Ahmed.Tyghri@hva.nl>
2017-07-14 01:33:36 +0200

    -created Test_Search_Company (Mockito) Test

--- src/test/java/SearchServiceTest.java
+++ src/test/java/SearchServiceTest.java
@@ -4,6 +4,7 @@ import com.uwetrottmann.tmdb2.entities.CompanyResultsPage;
 import com.uwetrottmann.tmdb2.services.SearchService;
 import org.junit.Test;
 import retrofit2.Call;
+import static org.mockito.Mockito.*;
 
 import java.io.IOException;
 
@@ -15,15 +16,30 @@ public class SearchServiceTest {
     private final String COMPANY_NAME = "Disney Pixar";
 
     Tmdb tmdb = new Tmdb(API_KEY);
-    SearchService searchService = tmdb.searchService();
+    SearchService service = tmdb.searchService();
 
     @Test
     public void Should_Find_Company() throws IOException {
-        Call<CompanyResultsPage> call = searchService.company(COMPANY_NAME, 1);
+        // Create call for our company
+        Call<CompanyResultsPage> call = service.company(COMPANY_NAME, 1);
+        // Get all company results
         CompanyResultsPage companyResult = call.execute().body();
+        // Search for our company in the results
         for(Company company : companyResult.results){
             assertEquals(company.name, COMPANY_NAME, COMPANY_NAME);
         }
+    }
+
+    @Test
+    public void Test_Search_Company() throws IOException {
+        // Mock SearchService
+        SearchService searchService = mock(SearchService.class);
+        // Mock Call<CompanyResultsPage>
+        Call<CompanyResultsPage> call = mock(Call.class);
+        // define return value for method company()
+        when(searchService.company(COMPANY_NAME,1)).thenReturn(call);
+        //assert that the call is the same for our Company
+        assertEquals(searchService.company(COMPANY_NAME,1), call);
     }
 
 }
