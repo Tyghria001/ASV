2af3e0783b7cfdc8514e34fc9d96810a31b027c2
Ahmed <Ahmed.Tyghri@hva.nl>
2017-07-13 23:41:42 +0200

    Init

---
+++ src/main/java/Driver.java
@@ -0,0 +1,30 @@
+import com.uwetrottmann.tmdb2.Tmdb;
+import com.uwetrottmann.tmdb2.entities.Movie;
+import com.uwetrottmann.tmdb2.entities.MovieKeywords;
+import com.uwetrottmann.tmdb2.entities.MovieResultsPage;
+import com.uwetrottmann.tmdb2.services.MoviesService;
+import com.uwetrottmann.tmdb2.services.SearchService;
+import retrofit2.Call;
+
+import java.io.IOException;
+
+public class Driver {
+
+    private static final String API_KEY = "b041e3dc3beef2b03b96c835b6b052ff";
+
+    public static void main(String[] args) throws IOException {
+
+        Tmdb tmdb = new Tmdb(API_KEY);
+
+        MoviesService movieService = tmdb.moviesService();
+        SearchService searchService = tmdb.searchService();
+
+        Call<MovieResultsPage> call = movieService.popular(1,"Disney");
+        MovieResultsPage results = call.execute().body();
+        for(Movie movie : results.results){
+            System.out.println(movie.title);
+        }
+
+
+    }
+}
