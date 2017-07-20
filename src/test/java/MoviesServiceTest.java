12b29fd3fd07a194506252ba82f2de2e8f4f611b
Ahmed <Ahmed.Tyghri@hva.nl>
2017-07-20 22:32:40 +0200

    -created Should_Find_Latest_Movie test

--- src/test/java/MoviesServiceTest.java
+++ src/test/java/MoviesServiceTest.java
@@ -1,6 +1,7 @@
 import com.uwetrottmann.tmdb2.Tmdb;
 import com.uwetrottmann.tmdb2.entities.Company;
 import com.uwetrottmann.tmdb2.entities.CompanyResultsPage;
+import com.uwetrottmann.tmdb2.entities.Movie;
 import com.uwetrottmann.tmdb2.entities.MovieResultsPage;
 import com.uwetrottmann.tmdb2.services.MoviesService;
 import com.uwetrottmann.tmdb2.services.SearchService;
@@ -11,6 +12,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -33,6 +35,29 @@ public class MoviesServiceTest {
         when(moviesService.popular(1, "Disney")).thenReturn(call);
         //assert that the call is the same for our Movie
         assertEquals(moviesService.popular(1, "Disney"), call);
+    }
+
+    @Test
+    public void Should_Find_Latest_Movie() throws IOException {
+        //Get the latest movie
+        MoviesService moviesService = tmdb.moviesService();
+        Movie latestMovie = moviesService.latest().execute().body();
+        //Search for the latest movie
+        SearchService searchService = tmdb.searchService();
+        Call<MovieResultsPage> call = searchService.movie(
+                latestMovie.title,
+                null,
+                latestMovie.original_language,
+                latestMovie.adult,
+                null,
+                null,
+                null
+        );
+        List<Movie> results = call.execute().body().results;
+        //Check if the latest movie is found
+        for (Movie foundMovie : results) {
+            assertEquals(foundMovie.title, latestMovie.title, latestMovie.title);
+        }
     }
 
 
